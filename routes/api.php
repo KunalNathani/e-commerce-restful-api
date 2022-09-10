e64d8ff37952a8a8d71963028a5bad54e9b6a27c
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-11 01:36:17 +0530

    added resourceful route for users

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('users', \App\Http\Controllers\User\UsersController::class);
 Route::resource('buyers', \App\Http\Controllers\Buyer\BuyersController::class)->only(['index', 'show']);
