65c1d02872572636215ad6427382c7d63d2ff75c
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-11 01:40:08 +0530

    added endpoints for buyers

--- app/Http/Controllers/Buyer/BuyersController.php
+++ app/Http/Controllers/Buyer/BuyersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Buyer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Buyer;
 use Illuminate\Http\Request;
 
 class BuyersController extends Controller
@@ -14,28 +15,12 @@ class BuyersController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        $buyers = Buyer::has('transactions')->get();
+        return response()
+                ->json([
+                    'data' => $buyers,
+                    'count'=> $buyers->count()
+                ], 200);
     }
 
     /**
@@ -46,40 +31,10 @@ class BuyersController extends Controller
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $buyer = Buyer::has('transactions')->findOrFail($id);
+        return response()
+                ->json([
+                    'data' => $buyer
+                ], 200);
     }
 }
