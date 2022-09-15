a8efbadda7ba553a50f0e3bf65fcce08c18d4eb1
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-15 23:58:24 +0530

    refactor: responses for buyers with ApiController

--- app/Http/Controllers/Buyer/BuyersController.php
+++ app/Http/Controllers/Buyer/BuyersController.php
@@ -2,39 +2,32 @@
 
 namespace App\Http\Controllers\Buyer;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 use App\Models\Buyer;
 use Illuminate\Http\Request;
 
-class BuyersController extends Controller
+class BuyersController extends ApiController
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
         $buyers = Buyer::has('transactions')->get();
-        return response()
-                ->json([
-                    'data' => $buyers,
-                    'count'=> $buyers->count()
-                ], 200);
+        return $this->showAll($buyers);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show($id)
     {
         $buyer = Buyer::has('transactions')->findOrFail($id);
-        return response()
-                ->json([
-                    'data' => $buyer
-                ], 200);
+        return $this->showOne($buyer);
     }
 }
