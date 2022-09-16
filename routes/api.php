16214d389e931c74bc1ed66761a4c09771ed1517
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-16 14:24:05 +0530

    added resourceful route for transactions

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::resource('users', \App\Http\Controllers\User\UsersController::class);
 Route::resource('buyers', \App\Http\Controllers\Buyer\BuyersController::class)->only(['index', 'show']);
 Route::resource('sellers', \App\Http\Controllers\Seller\SellersController::class)->only(['index', 'show']);
 Route::resource('products', \App\Http\Controllers\Product\ProductsController::class)->only(['index', 'show']);
+Route::resource('transactions', \App\Http\Controllers\Transaction\TransactionsController::class)->only(['index', 'show']);
