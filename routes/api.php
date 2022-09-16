2aed5605980e2f40bb8bb05fa7abe5e2b0212c38
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-16 14:15:40 +0530

    added resourceful route for products

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::resource('users', \App\Http\Controllers\User\UsersController::class);
 Route::resource('buyers', \App\Http\Controllers\Buyer\BuyersController::class)->only(['index', 'show']);
 Route::resource('sellers', \App\Http\Controllers\Seller\SellersController::class)->only(['index', 'show']);
+Route::resource('products', \App\Http\Controllers\Product\ProductsController::class)->only(['index', 'show']);
