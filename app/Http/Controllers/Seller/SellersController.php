8419f0ed0bd695c4494da0dca93c29293f5a4e72
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-16 10:51:19 +0530

    added global scope for seller queries

--- app/Http/Controllers/Seller/SellersController.php
+++ app/Http/Controllers/Seller/SellersController.php
@@ -15,7 +15,7 @@ class SellersController extends ApiController
      */
     public function index()
     {
-        $sellers = Seller::has('products')->get();
+        $sellers = Seller::all();
         return $this->showAll($sellers);
     }
 
--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\SellerScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,6 +10,12 @@ class Seller extends User
 {
     use HasFactory;
     protected $table = "users";
+
+    protected static function boot()
+    {
+        parent::boot();
+        self::addGlobalScope(new SellerScope());
+    }
 
     /**
     * RELATIONSHIP METHODS
