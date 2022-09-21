f08b4fba295c4b74af6fad705e0a43e49727041d
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-21 23:37:28 +0530

    added nested route and controller for buyer and sellers

---
+++ app/Http/Controllers/Buyer/BuyerSellersController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Http\Controllers\ApiController;
+use Illuminate\Http\Request;
+
+class BuyerSellersController extends ApiController
+{
+    //
+}
