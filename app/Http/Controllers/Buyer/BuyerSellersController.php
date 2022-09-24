c91df13d2e59b2d05af708c4de5299b856e60823
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-24 19:39:53 +0530

    added method in controller to get sellers associated with a buyer

--- app/Http/Controllers/Buyer/BuyerSellersController.php
+++ app/Http/Controllers/Buyer/BuyerSellersController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Buyer;
 
 use App\Http\Controllers\ApiController;
+use App\Models\Buyer;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class BuyerSellersController extends ApiController
 {
-    //
+    public function index(Buyer $buyer): JsonResponse
+    {
+        $sellers = $buyer->transactions()
+                        ->with('product.seller')
+                        ->get()
+                        ->pluck('product.seller')
+                        ->unique();
+        return $this->showAll($sellers);
+    }
 }
