8453ad71792803f5f735eb3309c05260bbc68d39
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-24 21:36:28 +0530

    added controller method to get buyers associated with a category

--- app/Http/Controllers/Category/CategoryBuyersController.php
+++ app/Http/Controllers/Category/CategoryBuyersController.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Controllers\Category;
 
+use App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class CategoryBuyersController extends Controller
+class CategoryBuyersController extends ApiController
 {
-    //
+    public function index(Category $category): JsonResponse
+    {
+        $buyers = $category->products()
+                            ->whereHas('transactions')
+                            ->with('transactions.buyer')
+                            ->get()
+                            ->pluck('transactions')
+                            ->flatten()
+                            ->pluck('buyer')
+                            ->unique()
+                            ->values();
+        return $this->showAll($buyers);
+    }
 }
