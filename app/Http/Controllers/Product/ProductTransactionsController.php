31114759ce6c430238924b775b2350f199a7a931
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-25 01:05:09 +0530

    added controller method to get transactions associated with a product

--- app/Http/Controllers/Product/ProductTransactionsController.php
+++ app/Http/Controllers/Product/ProductTransactionsController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers\Product;
 
+use App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class ProductTransactionsController extends Controller
+class ProductTransactionsController extends ApiController
 {
-    //
+    public function index(Product $product): JsonResponse
+    {
+        $transactions = $product->transactions;
+        return $this->showAll($transactions);
+    }
 }
