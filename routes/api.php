b3406ac0d16f10664fc08a954bdcc8c18fea86bd
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-24 21:34:09 +0530

    added controller and route to get buyers associated with a category

--- routes/api.php
+++ routes/api.php
@@ -29,3 +29,5 @@ Route::resource('buyers.categories', \App\Http\Controllers\Buyer\BuyerCategories
 Route::resource('categories.products', \App\Http\Controllers\Category\CategoryProductsController::class)->only(['index']);
 Route::resource('categories.sellers', \App\Http\Controllers\Category\CategorySellersController::class)->only(['index']);
 Route::resource('categories.transactions', \App\Http\Controllers\Category\CategoryTransactionsController::class)->only(['index']);
+Route::resource('categories.buyers', \App\Http\Controllers\Category\CategoryBuyersController::class)->only(['index']);
+
