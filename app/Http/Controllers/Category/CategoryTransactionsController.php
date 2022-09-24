f1cc50516df1d1b2ea5d0c67042f00dc5dd9574c
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-24 21:32:12 +0530

    added controller method to get transactions associated with a category

--- app/Http/Controllers/Category/CategoryTransactionsController.php
+++ app/Http/Controllers/Category/CategoryTransactionsController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers\Category;
 
+use App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class CategoryTransactionsController extends Controller
+class CategoryTransactionsController extends ApiController
 {
-    //
+    public function index(Category $category): JsonResponse
+    {
+        $transactions = $category->products()
+                                ->whereHas('transactions')
+                                ->with('transactions')
+                                ->get()
+                                ->pluck('transactions')
+                                ->flatten();
+        return $this->showAll($transactions);
+    }
 }
