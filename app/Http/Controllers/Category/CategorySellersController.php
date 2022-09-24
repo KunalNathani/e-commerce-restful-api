c0629931d3fcc26c97a636f4da8b00dc0f6b9b1e
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-24 21:22:54 +0530

    added controller method to get sellers associated with a category

--- app/Http/Controllers/Category/CategorySellersController.php
+++ app/Http/Controllers/Category/CategorySellersController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers\Category;
 
+use App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class CategorySellersController extends Controller
+class CategorySellersController extends ApiController
 {
-    //
+    public function index(Category $category): JsonResponse
+    {
+        $sellers = $category->products()
+                            ->with('seller')
+                            ->get()
+                            ->pluck('seller')
+                            ->unique()
+                            ->values();
+        return $this->showAll($sellers);
+    }
 }
