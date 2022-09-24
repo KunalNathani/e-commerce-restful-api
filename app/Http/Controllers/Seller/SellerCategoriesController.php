5d143ed643973c410d94d603442eacfbe93d6cad
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-24 22:01:03 +0530

    added controller method to get categories associated with a seller

--- app/Http/Controllers/Seller/SellerCategoriesController.php
+++ app/Http/Controllers/Seller/SellerCategoriesController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers\Seller;
 
+use App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
+use App\Models\Seller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class SellerCategoriesController extends Controller
+class SellerCategoriesController extends ApiController
 {
-    //
+    public function index(Seller $seller): JsonResponse
+    {
+        $categories = $seller->products()
+                            ->with('categories')
+                            ->get()
+                            ->pluck('categories')
+                            ->unique()
+                            ->flatten();
+        return $this->showAll($categories);
+    }
 }
