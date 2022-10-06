c0382640a38e152b1947efcfa7f6bec33791a6ef
Kunal Nathani <kunalnathani2042@gmail.com>
2022-10-06 21:20:43 +0530

    added method in user controller to resend mail

--- app/Http/Controllers/User/UsersController.php
+++ app/Http/Controllers/User/UsersController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\ApiController;
+use App\Mail\UserCreated;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class UsersController extends ApiController
 {
@@ -98,7 +100,7 @@ class UsersController extends ApiController
             }
         }
 
-        if($user->isDirty()) {
+        if(!$user->isDirty()) {
             return $this->errorResponse('You need to update some data!', 422);
         }
 
@@ -128,4 +130,13 @@ class UsersController extends ApiController
         return $this->showMessage("The account has been verified!");
     }
 
+    public function resend(User $user)
+    {
+        if($user->isVerified()) {
+            return $this->errorResponse('You are already verified!', 409);
+        }
+
+        Mail::to($user)->send(new UserCreated($user));
+        return $this->showMessage('Email Sent!');
+    }
 }
