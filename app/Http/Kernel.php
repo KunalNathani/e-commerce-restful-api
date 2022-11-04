edde5b7351b2a8de7b21d6df658a8624003bc0b1
Kunal Nathani <kunalnathani2042@gmail.com>
2022-11-04 21:31:32 +0530

    registered client credentials grant with route middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Http;
 
 use App\Http\Middleware\HeaderSignatureMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Laravel\Passport\Http\Middleware\CheckClientCredentials;
 
 class Kernel extends HttpKernel
 {
@@ -65,6 +66,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'signature' => HeaderSignatureMiddleware::class
+        'signature' => HeaderSignatureMiddleware::class,
+        'client.credentials' => CheckClientCredentials::class,
     ];
 }
