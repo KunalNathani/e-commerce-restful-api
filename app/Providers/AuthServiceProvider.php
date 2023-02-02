40b794e115fc2789e763da8ca9e3a1e6559b5e03
Kunal Nathani <kunalnathani2042@gmail.com>
2023-02-02 15:49:23 +0530

    fix: spelling error in scopes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,8 +31,8 @@ class AuthServiceProvider extends ServiceProvider
         Passport::personalAccessTokensExpireIn(now()->addMonths(6));
         Passport::tokensCan([
             'purchase-product' => 'Can create a Transaction',
-            'managed-prodcut' => "Create, Read, Update and Delete Product (Product CRUD)",
-            'managed-account' => "Read your account data such as id, name, email, is verified and is admin (cannot read password), modify your account details (email adn password only). CANNOT DELETE ACCOUNT!",
+            'manage-prodcut' => "Create, Read, Update and Delete Product (Product CRUD)",
+            'manage-account' => "Read your account data such as id, name, email, is verified and is admin (cannot read password), modify your account details (email and password only). CANNOT DELETE ACCOUNT!",
             'read-general' => "Read general information like purchasing categories, purchased products, selling products, selling categories, your transactions (purchases and sales)",
         ]);
     }
